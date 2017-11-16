c8f68f528f1c67c8b095ed7ccca83e4f8ca59f4f
uzeltinger <fabiouz@gmail.com>
2017-11-16 10:06:07 -0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -7,4 +7,6 @@ add_route('doctors', 'Doctors');
 add_route('doctor', 'Doctor');
 add_route('service', 'Service');
 add_route('patient-process', 'PatientProcess');
-// add_route('test', 'Test');
+add_route('stay', 'Stay');
+add_route('faq', 'Faq');
+add_route('contact', 'Contact');
