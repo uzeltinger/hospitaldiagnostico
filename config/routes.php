4205c3bed499e95217f61aeac221bb03e4b7ee4a
uzeltinger <fabiouz@gmail.com>
2017-11-16 09:00:45 -0300

    Update routes.php

--- config/routes.php
+++ config/routes.php
@@ -5,4 +5,5 @@ add_route('choose', 'Choose');
 add_route('installations', 'Installations');
 add_route('doctors', 'Doctors');
 add_route('doctor', 'Doctor');
+add_route('service', 'Service');
 // add_route('test', 'Test');
